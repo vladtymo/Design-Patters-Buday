1fd6b0fbcbf5ac260713a806c22009a9d42aba23
vladtymo <tymo.vlad@gmail.com>
2021-08-22 16:35:07 +0300

    Add Some Structural Pattern examples: Adapter, Bridge and Proxy

---
+++ 06_Adapter/Source.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class AmericanSocket
+{
+public:
+	virtual void MatchAmericanSocket()
+	{
+		cout << "USA - 220 V\n";
+	}
+};
+
+
+class EuroSocket
+{
+public:
+	virtual void MatchEuroSocket()
+	{
+		cout << "Euro - 220 V\n";
+	}
+};
+
+class ElectricityConsumer
+{
+public:
+	void Charge(EuroSocket* socket)
+	{
+		socket->MatchEuroSocket();
+	}
+};
+
+class Adapter : public EuroSocket
+{
+private:
+	AmericanSocket* usaS;
+public:
+
+	Adapter(AmericanSocket* usaSocket)
+	{
+		usaS = usaSocket;
+	}
+
+	virtual void MatchEuroSocket() override
+	{
+		usaS->MatchAmericanSocket();
+		// additional functionality
+	}
+};
+
+void main()
+{
+	// EuroSocket euroS;
+	AmericanSocket usaS;
+
+	ElectricityConsumer consumer;
+
+	Adapter ad(&usaS);
+
+	consumer.Charge(&ad);
+	consumer.Charge(new Adapter(&usaS));
+}
